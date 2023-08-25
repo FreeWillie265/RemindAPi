b4e3ad9df45178f4d9a7774b9fbd0dbe43d960b1
FreeWillie265 <wkasakula@gmail.com>
2023-08-25 15:52:51 +0200

    Feature: Added subject model

---
+++ RemindAPi/Models/Subject.cs
@@ -0,0 +1,13 @@
+﻿namespace RemindAPi.Models;
+
+public class Subject
+{
+    public Guid Id { get; set; }
+    public String AgeGroup { get; set; }
+    public String Sex { get; set; }
+    public String ClinicName { get; set; }
+    public String District { get; set; }
+    public String Clerk { get; set; }
+    public String Etc { get; set; }
+    public Boolean Traversed { get; set; }
+}
