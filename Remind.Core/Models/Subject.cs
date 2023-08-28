24c87b07b079019c0211e3e1797d10b525cbef5d
FreeWillie265 <wkasakula@gmail.com>
2023-08-28 22:20:51 +0200

    Feature: Add block id and size to Subject model

--- Remind.Core/Models/Subject.cs
+++ Remind.Core/Models/Subject.cs
@@ -3,11 +3,13 @@
 public class Subject
 {
     public Guid Id { get; set; }
-    public String AgeGroup { get; set; }
-    public String Sex { get; set; }
-    public String ClinicName { get; set; }
-    public String District { get; set; }
-    public String Clerk { get; set; }
-    public String Etc { get; set; }
+    public string AgeGroup { get; set; }
+    public string Sex { get; set; }
+    public string BlockId { get; set; }
+    public int BlockSize { get; set; }
+    public string ClinicName { get; set; }
+    public string District { get; set; }
+    public string Clerk { get; set; }
+    public string Etc { get; set; }
     public Boolean Traversed { get; set; }
 }
