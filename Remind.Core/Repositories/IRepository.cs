11cb458dfcb0e6ca144f94416bf58ec314f3526a
FreeWillie265 <wkasakula@gmail.com>
2023-08-28 22:21:23 +0200

    Feature: Added generic repository

---
+++ Remind.Core/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Remind.Core.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    ValueTask<TEntity> GetByIdAsync(Guid id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression);
+    Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> expression);
+    Task AddAsync(TEntity entity);
+    Task AddRangeAsync(IEnumerable<TEntity> entities);
+    void Remove(TEntity entity);
+    void RemoveRange(IEnumerable<TEntity> entities);
+}
