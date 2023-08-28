64a29965b13ed3ec9a426b5be00532c522a7ffb3
FreeWillie265 <wkasakula@gmail.com>
2023-08-28 22:48:39 +0200

    Feature: Add a db context

---
+++ Remind.Data/SubjectDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Remind.Core.Models;
+using Remind.Data.Configurations;
+
+namespace Remind.Data;
+
+public class SubjectDbContext : DbContext
+{
+    public DbSet<Subject> Subjects { get; set; }
+
+    public SubjectDbContext(DbContextOptions<SubjectDbContext> options)
+    : base(options)
+    {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new SubjectConfiguration());
+    }
+}
