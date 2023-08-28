85c184cf35b353d4c647b54febf31167c2c1c40c
FreeWillie265 <wkasakula@gmail.com>
2023-08-28 22:43:26 +0200

    Feature: Add Subject configuration class

---
+++ Remind.Data/Configurations/SubjectConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Remind.Core.Models;
+
+namespace Remind.Data.Configurations;
+
+public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
+{
+    public void Configure(EntityTypeBuilder<Subject> builder)
+    {
+    }
+}
