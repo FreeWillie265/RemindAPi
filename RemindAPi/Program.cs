4b862dc1aeb2fccc716fa04c7b7a7649099da6fc
FreeWillie265 <wkasakula@gmail.com>
2023-08-28 23:20:35 +0200

    Feature: Add dependency injection for the Unit of Work

--- RemindAPi/Program.cs
+++ RemindAPi/Program.cs
@@ -1,3 +1,6 @@
+using Remind.Core.Repositories;
+using Remind.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
