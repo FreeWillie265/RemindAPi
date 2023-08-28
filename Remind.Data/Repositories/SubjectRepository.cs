bb1647bdca170e9a5f578a6df1d09c7e2f944baf
FreeWillie265 <wkasakula@gmail.com>
2023-08-28 23:08:19 +0200

    Feature: Implemented Subject Repository

---
+++ Remind.Data/Repositories/SubjectRepository.cs
@@ -0,0 +1,11 @@
+﻿using Remind.Core.Models;
+using Remind.Core.Repositories;
+
+namespace Remind.Data.Repositories;
+
+public class SubjectRepository : Repository<Subject>, ISubjectRepository
+{
+    public SubjectRepository(SubjectDbContext context) : base(context)
+    {}
+    
+}
