a5559da015a24bbefa709da24fcc6d0032b376ad
FreeWillie265 <wkasakula@gmail.com>
2023-08-28 22:27:55 +0200

    Feature: Add Subject Service interface

---
+++ Remind.Core/Services/ISubjectService.cs
@@ -0,0 +1,12 @@
+﻿using Remind.Core.Models;
+
+namespace Remind.Core.Services;
+
+public interface ISubjectService
+{
+    Task<Subject> GetById(Guid id);
+    Task<IEnumerable<Subject>> GetAll();
+    Task<Subject> Create(Subject subject);
+    Task UpdateSubject(Subject subjectToBeUpdated, Subject subject);
+    Task DeleteSubject(Subject subject);
+}
