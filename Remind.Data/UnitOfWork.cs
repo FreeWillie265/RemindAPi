ce95e498837f2f0834cc204f8d3b6aec304a8aeb
FreeWillie265 <wkasakula@gmail.com>
2023-08-28 23:14:47 +0200

    Feature: Implement Unit of work

---
+++ Remind.Data/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Remind.Core.Repositories;
+using Remind.Data.Repositories;
+
+namespace Remind.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly SubjectDbContext _context;
+    private SubjectRepository _subjectRepository;
+
+    public UnitOfWork(SubjectDbContext context)
+    {
+        this._context = context;
+    }
+
+    public ISubjectRepository Subjects => _subjectRepository
+        = _subjectRepository ?? new SubjectRepository(_context);
+
+    public async Task<int> CommitAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
