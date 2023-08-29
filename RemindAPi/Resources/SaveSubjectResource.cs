46f1d933b9f63f4dca19b7ada4f61f1f8d2bad10
FreeWillie265 <wkasakula@gmail.com>
2023-08-29 23:52:23 +0200

    Feature: Add a model used to update a subject

---
+++ RemindAPi/Resources/SaveSubjectResource.cs
@@ -0,0 +1,14 @@
+﻿namespace RemindAPi.Resources;
+
+public class SaveSubjectResource
+{
+    public string AgeGroup { get; set; }
+    public string Sex { get; set; }
+    public string BlockId { get; set; }
+    public int BlockSize { get; set; }
+    public string ClinicName { get; set; }
+    public string District { get; set; }
+    public string Clerk { get; set; }
+    public string Etc { get; set; }
+    public Boolean Traversed { get; set; }
+}
