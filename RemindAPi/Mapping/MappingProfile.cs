262898d8601aa1bbf568fd9f463e809f03989f4b
FreeWillie265 <wkasakula@gmail.com>
2023-08-30 00:03:18 +0200

    Feature: Add automapper profile and mapped save subject resource to the subject object

---
+++ RemindAPi/Mapping/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Remind.Core.Models;
+using RemindAPi.Resources;
+
+namespace RemindAPi.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<SaveSubjectResource, Subject>();
+    }
+}
