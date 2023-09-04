bc5cfd598a1b555e5b18991657d74baf652a75c0
FreeWillie265 <wkasakula@gmail.com>
2023-09-04 19:24:06 +0200

    Feature: Made the non-required Subject field nullable

--- Remind.Core/Models/Subject.cs
+++ Remind.Core/Models/Subject.cs
@@ -7,9 +7,9 @@ public class Subject
     public string Sex { get; set; }
     public string BlockId { get; set; }
     public int BlockSize { get; set; }
-    public string ClinicName { get; set; }
-    public string District { get; set; }
-    public string Clerk { get; set; }
-    public string Etc { get; set; }
+    public string? ClinicName { get; set; }
+    public string? District { get; set; }
+    public string? Clerk { get; set; }
+    public string? Etc { get; set; }
     public Boolean Traversed { get; set; }
 }
---
+++ Remind.Data/Migrations/20230904172206_MakeColumnsNullable.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Remind.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeColumnsNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Etc",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "District",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClinicName",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Clerk",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Etc",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "District",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClinicName",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Clerk",
+                table: "Subjects",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
