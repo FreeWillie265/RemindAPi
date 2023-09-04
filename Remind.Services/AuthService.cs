0e87f59c310f878e0a3bae0c321f942c269ad3c8
FreeWillie265 <wkasakula@gmail.com>
2023-09-04 10:26:23 +0200

    Fix: Outputting the actual error when a registration fails

--- Remind.Services/AuthService.cs
+++ Remind.Services/AuthService.cs
@@ -14,13 +14,15 @@ public class AuthService : IAuthService
     private readonly UserManager<ApplicationUser> userManager;
     private readonly RoleManager<IdentityRole> roleManager;
     private readonly IConfiguration _configuration;
-    public AuthService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+
+    public AuthService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration)
     {
         this.userManager = userManager;
         this.roleManager = roleManager;
         _configuration = configuration;
-
     }
+
     public async Task<(int, string)> Registration(RegistrationModel model, string role)
     {
         var userExists = await userManager.FindByNameAsync(model.Username);
@@ -37,7 +39,7 @@ public class AuthService : IAuthService
         };
         var createUserResult = await userManager.CreateAsync(user, model.Password);
         if (!createUserResult.Succeeded)
-            return (0,"User creation failed! Please check user details and try again.");
+            return (0, createUserResult.Errors.First().Description);
 
         if (!await roleManager.RoleExistsAsync(role))
             await roleManager.CreateAsync(new IdentityRole(role));
@@ -45,7 +47,7 @@ public class AuthService : IAuthService
         if (await roleManager.RoleExistsAsync(role))
             await userManager.AddToRoleAsync(user, role);
 
-        return (1,"User created successfully!");
+        return (1, "User created successfully!");
     }
 
     public async Task<(int, string)> Login(LoginModel model)
@@ -55,7 +57,7 @@ public class AuthService : IAuthService
             return (0, "Invalid username");
         if (!await userManager.CheckPasswordAsync(user, model.Password))
             return (0, "Invalid password");
-            
+
         var userRoles = await userManager.GetRolesAsync(user);
         var authClaims = new List<Claim>
         {
@@ -67,10 +69,11 @@ public class AuthService : IAuthService
         {
             authClaims.Add(new Claim(ClaimTypes.Role, userRole));
         }
+
         string token = GenerateToken(authClaims);
         return (1, token);
     }
-    
+
     private string GenerateToken(IEnumerable<Claim> claims)
     {
         var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTKey:Secret"]));
