5c970de80856b99928e7cdda67a5ed1263535e5a
FreeWillie265 <wkasakula@gmail.com>
2023-09-13 10:14:55 +0200

    Feature: Create migrations to add columns

---
+++ Remind.Data/Migrations/20230913081007_AddDataIdAndAssigned.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Remind.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataIdAndAssigned : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Traversed",
+                table: "Subjects",
+                newName: "Assigned");
+
+            migrationBuilder.RenameColumn(
+                name: "Etc",
+                table: "Subjects",
+                newName: "Note");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DataId",
+                table: "Subjects",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataId",
+                table: "Subjects");
+
+            migrationBuilder.RenameColumn(
+                name: "Note",
+                table: "Subjects",
+                newName: "Etc");
+
+            migrationBuilder.RenameColumn(
+                name: "Assigned",
+                table: "Subjects",
+                newName: "Traversed");
+        }
+    }
+}
