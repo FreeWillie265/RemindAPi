9af10a41192a20797417ba0c5c502a854d7f990d
FreeWillie265 <wkasakula@gmail.com>
2024-06-21 20:58:13 +0200

    Add: Create observation DTO

---
+++ RemindAPi/Resources/ObservationResource.cs
@@ -0,0 +1,11 @@
+namespace RemindAPi.Resources;
+
+public class ObservationResource
+{
+    public String DataId { get; set; }
+    public String Stratum { get; set; }
+    public String BlockId { get; set; }
+    public int BlockSize { get; set; }
+    public String Treatment { get; set; }
+    public Boolean Assigned { get; set; }
+}
