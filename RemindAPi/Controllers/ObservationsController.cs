c0970c9a8c36f6538b50d16801cb81d99fdf04fa
FreeWillie265 <wkasakula@gmail.com>
2024-06-21 20:58:52 +0200

    Add: Create download observations endpoint

--- RemindAPi/Controllers/ObservationsController.cs
+++ RemindAPi/Controllers/ObservationsController.cs
@@ -1,9 +1,13 @@
+using System.Globalization;
 using AutoMapper;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Remind.Core.Models;
 using Remind.Core.Services;
+using RemindAPi.Resources;
 
 namespace RemindAPi.Controllers
 {
@@ -42,5 +46,28 @@ namespace RemindAPi.Controllers
             var processedObservation = await service.ProcessObservation(observation);
             return Ok(processedObservation);
         }
+
+        [HttpGet("download-v2-data")]
+        public async Task<IActionResult> DownloadObservationData()
+        {
+            var observations = await service.GetAll();
+            var observationResources = mapper
+                .Map<List<Observation>, List<ObservationResource>>(observations.ToList());
+            string filePath = $"remind-{DateTime.Now:dd-MMMM-yyyy}.csv";
+
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                await csv.WriteRecordsAsync(observationResources);
+            }
+
+            byte[] fileContents = System.IO.File.ReadAllBytes(filePath);
+            string filename = Path.GetFileName(filePath);
+
+            FileContentResult fileContentResult = File(fileContents, "text/csv", filename);
+            System.IO.File.Delete(filePath);
+            return fileContentResult;
+        }
+        
     }
 }
